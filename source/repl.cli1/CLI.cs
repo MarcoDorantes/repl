61ceda3e2862a52a760fce791a039de36622be08
marcodorantes <marcodorantes@hotmail.com>
2019-09-07 15:23:40 -0500

    Added CLI_1_BasicCases

--- source/repl.cli1/CLI.cs
+++ source/repl.cli1/CLI.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
 
 namespace repl.cli1
 {
+    public class RootInput { }
+
     public class CLI
     {
-        public static void MainEntryPoint(string[] args)
-        { }
+        public static void MainEntryPoint(string[] args, TextWriter writer = null)
+        {
+            try
+            {
+                if (writer == null)
+                {
+                    writer = Console.Out;
+                }
+                if (Environment.UserInteractive)
+                {
+                    writer.WriteLine($"Hello, {Environment.UserDomainName}\\{Environment.UserName} !!!");
+                    writer.WriteLine($"PID: {System.Diagnostics.Process.GetCurrentProcess().Id} Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} Culture: {System.Threading.Thread.CurrentThread?.CurrentUICulture?.Name}, {System.Threading.Thread.CurrentThread?.CurrentCulture?.Name}\n");
+                }
+                if (!(args?.Any() == true) || args?.First().Contains("?") == true)
+                {
+                    writer.WriteLine($"Working with {GetHostProcessName()}:");
+                    nutility.Switch.ShowUsage(typeof(RootInput));
+                }
+                else
+                {
+                    nutility.Switch.AsType<RootInput>(args);
+                }
+            }
+            catch (Exception ex)
+            {
+                for (int level = 0; ex != null; ex = ex.InnerException, ++level)
+                {
+                    writer.WriteLine($"\r\n[Level {level}] {ex.GetType().FullName}: {ex.Message} {ex.StackTrace}");
+                }
+            }
+
+            string GetHostProcessName()
+            {
+                var result = Environment.GetCommandLineArgs()?.FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(result))
+                {
+                    result = System.IO.Path.GetFileNameWithoutExtension(result);
+                }
+                return result;
+            }
+        }
     }
 }
--- source/repl.spec/BasicCases.cs
+++ source/repl.spec/BasicCases.cs
@@ -41,7 +41,32 @@ namespace repl.spec
             //Assert
             Assert.IsTrue($"{writer}".Contains("x1"));
         }
+        private string asTextContent(IEnumerable<string> lines) => $"{lines?.Aggregate(new StringWriter(), (whole, next) => { whole.WriteLine(next); return whole; })}";
+    }
 
+    [TestClass]
+    public class CLI_1_BasicCases
+    {
+        [TestMethod]
+        public void basic()
+        {
+            //Arrange
+            var inputX_instance = new cli1.RootInput();
+            var current_level = new nutility.InputReplLevel<cli1.RootInput>();
+            current_level.ClassChilds.Add("c2", typeof(cli1.RootInput));
+            current_level.Instances.Add("x1", inputX_instance);
+
+            var input_lines = new[] { "d" };
+            var reader = new StringReader(asTextContent(input_lines));
+            var writer = new StringWriter();
+            var repl = new nutility.REPL<cli1.RootInput> { Reader = reader, Writer = writer };
+
+            //Act
+            repl.Loop(current_level);
+
+            //Assert
+            Assert.IsTrue($"{writer}".Contains("x1"));
+        }
         private string asTextContent(IEnumerable<string> lines) => $"{lines?.Aggregate(new StringWriter(), (whole, next) => { whole.WriteLine(next); return whole; })}";
     }
 }
